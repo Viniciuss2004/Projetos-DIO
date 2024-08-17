00069731656da7e4f91c8b32e1af4c25f77f2fc7
Viniciuss2004 <viandcustodio@gmail.com>
2024-08-17 15:23:27 -0300

    Concluindo a criação das classes do domínio

--- src/Dominio/Dev.java
+++ src/Dominio/Dev.java
@@ -2,6 +2,7 @@ package Dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,11 +11,24 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluido = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp) {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir() {}
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluido.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-    public void calcularTotalXp() {}
+    public double calcularTotalXp() {
+       return this.conteudosInscritos.stream().mapToDouble(Conteudo::calcularXP).sum();
+    }
 
     public String getNome() {
         return nome;
